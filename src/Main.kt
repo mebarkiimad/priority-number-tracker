6866766cc8e464d046c9c59bba1e3deb66e6723d
Mebarki ahmed imad eddine <mebarkiahmedimadeddine@gmail.com>
2025-02-13 13:28:42 +0100

    Add channel sending series

--- src/Main.kt
+++ src/Main.kt
@@ -4,19 +4,29 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main(): Unit = runBlocking {
-    val dataChannel = Channel<Int>()
+    val dataChannel = Channel<MutableList<Int>>()
     val producerJob = launch {
-        repeat(10000) { // launch a lot of coroutines
-            launch {
-                delay(500L)
+        val serie  = mutableListOf<Int>()
+        var TIMER = 0;
+        repeat(2  ) {
+            repeat(5) {
+                delay(200L)
                 val randomClassNumber = (0..5).random()
-                dataChannel.send(randomClassNumber)
+                serie.add(randomClassNumber)
+                TIMER += 1;
+                if (TIMER == 5) {
+                    dataChannel.send(serie)
+                    TIMER = 0;
+
+                }
             }
         }
     }
     launch {
         for (received in dataChannel) {
-            println("Received: $received")
+            val message = received.joinToString(",")
+            println("Received: [$message]")
         }
+        println("End")
     }
 }
